ba51edc0a7300ed029269805f1aa12d207c0ea30
Andrew Davisson <akdavisson4@gmail.com>
2019-07-21 17:15:21 -0600

    Added login functionality

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,17 +1,35 @@
 class UserController < ApplicationController
 
   get '/login' do
-    erb :'/users/login'
+    if logged_in?
+      redirect '/books'
+    else
+      erb :'/users/login'
+    end
   end
 
   get '/signup' do
-    erb :'/users/signup'
+    if logged_in?
+      redirect '/books'
+    else
+      erb :'/users/signup'
+    end
   end
 
   get '/logout' do
     if logged_in?
       session.clear
       redirect '/'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(name: params[:name])
+    if user.name != "" && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/books'
     else
       redirect '/login'
     end
