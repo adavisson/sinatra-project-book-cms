a0094d2054c4506b7d7e9ba8bdf35835e83113d0
Andrew Davisson <akdavisson4@gmail.com>
2019-07-21 13:21:09 -0600

    first commit

---
+++ lib/sinatra/project/book/cms.rb
@@ -0,0 +1,12 @@
+require "sinatra/project/book/cms/version"
+
+module Sinatra
+  module Project
+    module Book
+      module Cms
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/sinatra/project/book/cms/version.rb
@@ -0,0 +1,9 @@
+module Sinatra
+  module Project
+    module Book
+      module Cms
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
